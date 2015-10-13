dc6031d39e939b44adc765d248a000156a8d6d5f
Gleb Maliborski <vynipox@gmail.com>
2015-10-14 00:38:24 +0300

    Lesson 13. With synchronization. Commit.

---
+++ Lesson_13/WithSynchronization/src/com/vynipox/glm/Main.java
@@ -0,0 +1,62 @@
+/**
+ * Main class.
+ */
+package com.vynipox.glm;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Variables declaration.
+		Scanner scanner = new Scanner(System.in);
+		String input = "";
+		boolean check = false;
+		
+		PrintValues printValues = new PrintValues();
+		
+		MyThread myThread = new MyThread(printValues);
+		MyThread myThread1 = new MyThread(printValues);
+		
+		System.out.println("Enter the start!");
+
+		while(!check)
+		{
+			input = scanner.nextLine();
+			
+			if(input.toLowerCase().equals("exit"))
+			{
+				System.exit(-1);
+			}
+			else if(input.toLowerCase().equals("start"))
+			{
+					print(myThread);
+					print(myThread1);
+		
+				try {
+					myThread.join();
+					myThread1.join();
+				} catch (InterruptedException e) {
+					System.out.println(e.getMessage());
+				}
+				System.exit(-1);
+			}
+			else
+			{
+				System.out.println("Wrong arguments, try again!");
+			}
+		}
+		
+		scanner.close();
+	}
+	
+	/**
+	 * Print method.
+	 * @param thread
+	 */
+	public static void print(MyThread thread)
+	{
+		thread.start();
+	}
+}
